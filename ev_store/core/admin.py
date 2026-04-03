567f5850cc6e189d970656a4051161f416440090
MrQuangThien <thiencute9133@gmail.com>
2026-04-03 21:38:55 +0700

    Delete ev_store directory

--- ev_store/core/admin.py
+++
@@ -1,8 +0,0 @@
-
-from django.contrib import admin
-from .models import *
-
-admin.site.register(CuaHang)
-admin.site.register(XeDien)
-admin.site.register(TramSac)
-admin.site.register(PhienSac)
--- ev_store/core/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from .views import ban_do_tram_sac
-
-urlpatterns = [
-    path("map/", ban_do_tram_sac),
-]
